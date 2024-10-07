117878d3ab20df8528e34c92f9d10fbb71565305
Omar Ghoz <133299432+OmarGhoz@users.noreply.github.com>
2024-10-07 09:58:28 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,4 +13,5 @@ class BankAccount:
         return True
     
     def display_balance(self):
-        print(f"Current Balance: ${self._account_balance}")
+    print(f"Current Balance: ${self._account_balance:.2f}")
+
