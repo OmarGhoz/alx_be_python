1f32cc6113fd6dbbaab97c7bbfa362d940099f2f
Omar Ghoz <133299432+OmarGhoz@users.noreply.github.com>
2024-10-07 10:15:48 +0300

    Create library_management.py

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,27 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        """Mark the book as checked out."""
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        """Mark the book as returned (available)."""
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+
+    def is_available(self):
+        """Return whether the book is available."""
+        return not self._is_checked_out
+
+    def __str__(self):
+        """Return a string representation of the book."""
+        return f"{self.title} by {self.author}"
